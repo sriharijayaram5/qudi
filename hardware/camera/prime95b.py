631187a50c3b97481f221521c29960fd1d213aab
sriharijayaram5 <sriharijayaram5@gmail.com>
2020-12-02 09:48:49 +0100

    Changed clear mode to Pre exp in pulsed for cam

--- hardware/camera/prime95b.py
+++ hardware/camera/prime95b.py
@@ -353,6 +353,7 @@ class Prime95B(Base, CameraInterface, FastCounterInterface):
         mode = 1 #EXPOSE_OUT_ALL_ROWS
         mode = 3 #MAX
         self.cam.exp_out_mode = mode
+        self.cam.clear_mode = 'Pre-Exposure'
         if self.pulsed_frames is None:
             self.pulsed_frames = np.zeros(no_of_laser_pulses, dtype='float32')
     
@@ -361,6 +362,7 @@ class Prime95B(Base, CameraInterface, FastCounterInterface):
         self.set_exposure_mode("Ext Trig Internal")
         mode = 2 #EXPOSE_OUT_ANY_ROWS
         self.cam.exp_out_mode = mode
+        self.cam.clear_mode = 'Post-Sequence'
         self.pulsed_frames = None
     
     def configure(self, bin_width_s, record_length_s, number_of_gates=0):
